f20553eb164157aea14cf9352e6a2de3cccbbc1e
Dawid <dawidj119@interia.eu>
2021-11-27 15:09:19 +0100

    brakujaceg pliki do projektu

--- Topics/Array exceptions/Calculate Square/src/Main.java
+++ Topics/Array exceptions/Calculate Square/src/Main.java
@@ -2,6 +2,11 @@ class FixingExceptions {
 
     public static void calculateSquare(int[] array, int index) {
         // write your code here
+        if(array!=null && index<array.length && index>-1){
+            System.out.println(array[index]*array[index]);
+        }else{
+            System.out.println("Exception!");
+        }
 
     }
 }
--- Topics/Array exceptions/Out of bounds_/src/Main.java
+++ Topics/Array exceptions/Out of bounds_/src/Main.java
@@ -8,6 +8,11 @@ class FixingStringIndexOutOfBoundsException {
         String string = scanner.nextLine();
         int index = scanner.nextInt();
 
-        System.out.println(string.charAt(index));
+        if(index<0 || index>string.length()-1){
+            System.out.println("Out of bounds!");
+        }else{
+            System.out.println(string.charAt(index));
+        }
+
     }
 }
